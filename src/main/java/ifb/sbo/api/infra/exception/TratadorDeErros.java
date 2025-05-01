5f6f6dcfe740d559087eb52d90536c5f8e56776a
Mateus Itacarambi <mateusdesouzaitacarambi@gmail.com>
2025-05-01 20:42:04 -0300

    Atualizações

--- src/main/java/ifb/sbo/api/infra/exception/TratadorDeErros.java
+++ src/main/java/ifb/sbo/api/infra/exception/TratadorDeErros.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -43,5 +44,10 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFound(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Usuário não encontrado!");
     }
 }
